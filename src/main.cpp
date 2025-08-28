05ac791046fef91a8ffd3a0a9cb89efad78a4288
Elikplim-N <ekn2526@gmail.com>
2025-08-28 13:05:11 +0000

    added bme sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <bme68x.h>
+
+//create your bme class
 
 // put function declarations here:
 int myFunction(int, int);
@@ -6,10 +9,13 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  //initiliase bme sensor
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // read bme sensor data
 }
 
 // put function definitions here:
